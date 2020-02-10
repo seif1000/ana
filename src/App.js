bc3b3dfac6843aac76f8ac9b49a3c941d8a2378f
seif1000 <seifwin74@gmail.com>
2020-02-10 22:35:04 +0100

    manifest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React ,{useState,useEffect} from 'react';
+import React ,{useEffect} from 'react';
 import './App.scss';
 import ShowCase from './componnet/ShowCase';
 import Skill from './componnet/Skill';
 import Project from './componnet/Project';
 import Footer from './componnet/Footer';
-import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
-import {database ,initializeApp} from "firebase" ;
+import {  Element , animateScroll as scroll } from 'react-scroll'
+import {initializeApp} from "firebase" ;
 function App() {
   const config = {
     apiKey: "AIzaSyAdbxi8lgWr_d6V9oQY-zbEyNJFn497XUw",
--- src/componnet/Footer.js
+++ src/componnet/Footer.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react' ;
-import {database ,initializeApp} from "firebase" ;
-import firebase from "firebase" ;
+import {database } from "firebase" ;
+
 
 
 export default function Footer() {
